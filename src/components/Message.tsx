7a44ac50e59fd64a4a58db5ebdf824ab19dfa168
jslee2023 <130067275+jslee2023@users.noreply.github.com>
2025-10-03 14:40:38 +0100

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -68,7 +68,7 @@ const MessageComponent: React.FC<MessageProps> = React.memo(({ message }) => { /
 
   const messageClasses = isUser
     ? 'bg-blue-500/80 self-end rounded-br-none'
-    : 'bg-yellow-500/90 dark:bg-yellow-500/90 self-start rounded-tl-none';
+    : 'bg-yellow-900/90 dark:bg-yellow-900/90 self-start rounded-tl-none';
 
   const containerClasses = `flex items-end ${isUser ? 'justify-end' : 'justify-start'} gap-2 mb-3`;
 
